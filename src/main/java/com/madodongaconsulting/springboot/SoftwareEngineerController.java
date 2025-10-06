dac459641197b2892f01b5e46e41ec719aba9717
madodongais <51439355+madodongais@users.noreply.github.com>
2025-10-06 17:47:33 +0200

    Initial commit

---
+++ src/main/java/com/madodongaconsulting/springboot/SoftwareEngineerController.java
@@ -0,0 +1,24 @@
+package com.madodongaconsulting.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController {
+
+    private final SoftwareEngineerService softwareEngineerService;
+
+    public SoftwareEngineerController(SoftwareEngineerService softwareEngineerService) {
+        this.softwareEngineerService = softwareEngineerService;
+    }
+
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers() {
+        return softwareEngineerService.getAllSoftwareEngineers();
+    }
+}
+
